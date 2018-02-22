34fd05582883a1154d43f697113bb595df5ec7ae
i7676131 <i7676131@bournemouth.ac.uk>
2018-02-22 20:19:35 +0000

    Created main slide page with minor backend functionality.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var socialSlide = require('./routes/social-slide')
 
 var isProd = process.env.NODE_ENV === 'production';
 console.log("Prod? "+isProd);
@@ -28,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/social-slide', socialSlide);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/social-slide.js
+++ routes/social-slide.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('social-slide', {imageSrc: ''});
+
+
+    res.render('social-slide', {
+        source: getNextImage(),
+        title: 'Social Slide',
+        smName: 'Tyrone Williams',
+        smUsername: '@Mr_Rone_',
+        smPostContent: 'Beach walk after eating at @HarryRamsdensUk, perfect. #FishAndChips #Beach #BeachWalks #Bournemouth' +
+        ' #Home #Sunset',
+        smDate: '25 Jan 2018',
+        smAvatar: '/images/profile_pic.jpg',
+        smLogo: '/images/logos/Twitter_Logo_Blue.png'
+    });
 });
+
+function getNextImage(){
+    return '/images/slide_image1.jpg'
+}
 
 module.exports = router;
